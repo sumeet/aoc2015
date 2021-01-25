bff41c4efd0a400a803906b3092da169d2a5d30c
Sumeet Agarwal <sumeet.a@gmail.com>
2021-01-24 16:30:36 -0800

    finish day13 part1

--- day13/src/Lib.hs
+++ day13/src/Lib.hs
@@ -3,14 +3,14 @@
 module Lib where
 
 import Data.List (permutations)
-import Data.Map (Map, fromListWith, keys)
+import Data.Map (Map, fromList, fromListWith, keys, (!))
 import Text.RawString.QQ
 
 type Person = String
 
-data SeatmateQuality = SeatmateQuality Person Int deriving (Show)
+type SeatmateQuality = Map Person Int
 
-type Preferences = Map Person [SeatmateQuality]
+type Preferences = Map Person SeatmateQuality
 
 sample :: String
 sample =
@@ -27,10 +27,69 @@ David would gain 46 happiness units by sitting next to Alice.
 David would lose 7 happiness units by sitting next to Bob.
 David would gain 41 happiness units by sitting next to Carol.|]
 
-preferences :: Preferences
-preferences = fromListWith (<>) $ map parseLine $ lines sample
+input :: String
+input =
+  [r|Alice would gain 2 happiness units by sitting next to Bob.
+Alice would gain 26 happiness units by sitting next to Carol.
+Alice would lose 82 happiness units by sitting next to David.
+Alice would lose 75 happiness units by sitting next to Eric.
+Alice would gain 42 happiness units by sitting next to Frank.
+Alice would gain 38 happiness units by sitting next to George.
+Alice would gain 39 happiness units by sitting next to Mallory.
+Bob would gain 40 happiness units by sitting next to Alice.
+Bob would lose 61 happiness units by sitting next to Carol.
+Bob would lose 15 happiness units by sitting next to David.
+Bob would gain 63 happiness units by sitting next to Eric.
+Bob would gain 41 happiness units by sitting next to Frank.
+Bob would gain 30 happiness units by sitting next to George.
+Bob would gain 87 happiness units by sitting next to Mallory.
+Carol would lose 35 happiness units by sitting next to Alice.
+Carol would lose 99 happiness units by sitting next to Bob.
+Carol would lose 51 happiness units by sitting next to David.
+Carol would gain 95 happiness units by sitting next to Eric.
+Carol would gain 90 happiness units by sitting next to Frank.
+Carol would lose 16 happiness units by sitting next to George.
+Carol would gain 94 happiness units by sitting next to Mallory.
+David would gain 36 happiness units by sitting next to Alice.
+David would lose 18 happiness units by sitting next to Bob.
+David would lose 65 happiness units by sitting next to Carol.
+David would lose 18 happiness units by sitting next to Eric.
+David would lose 22 happiness units by sitting next to Frank.
+David would gain 2 happiness units by sitting next to George.
+David would gain 42 happiness units by sitting next to Mallory.
+Eric would lose 65 happiness units by sitting next to Alice.
+Eric would gain 24 happiness units by sitting next to Bob.
+Eric would gain 100 happiness units by sitting next to Carol.
+Eric would gain 51 happiness units by sitting next to David.
+Eric would gain 21 happiness units by sitting next to Frank.
+Eric would gain 55 happiness units by sitting next to George.
+Eric would lose 44 happiness units by sitting next to Mallory.
+Frank would lose 48 happiness units by sitting next to Alice.
+Frank would gain 91 happiness units by sitting next to Bob.
+Frank would gain 8 happiness units by sitting next to Carol.
+Frank would lose 66 happiness units by sitting next to David.
+Frank would gain 97 happiness units by sitting next to Eric.
+Frank would lose 9 happiness units by sitting next to George.
+Frank would lose 92 happiness units by sitting next to Mallory.
+George would lose 44 happiness units by sitting next to Alice.
+George would lose 25 happiness units by sitting next to Bob.
+George would gain 17 happiness units by sitting next to Carol.
+George would gain 92 happiness units by sitting next to David.
+George would lose 92 happiness units by sitting next to Eric.
+George would gain 18 happiness units by sitting next to Frank.
+George would gain 97 happiness units by sitting next to Mallory.
+Mallory would gain 92 happiness units by sitting next to Alice.
+Mallory would lose 96 happiness units by sitting next to Bob.
+Mallory would lose 51 happiness units by sitting next to Carol.
+Mallory would lose 81 happiness units by sitting next to David.
+Mallory would gain 31 happiness units by sitting next to Eric.
+Mallory would lose 73 happiness units by sitting next to Frank.
+Mallory would lose 89 happiness units by sitting next to George.|]
 
-parseLine :: String -> (Person, [SeatmateQuality])
+preferences :: Preferences
+preferences = fromListWith (<>) $ map parseLine $ lines input
+
+parseLine :: String -> (Person, SeatmateQuality)
 parseLine s =
   let s' = init s -- remove "." from end of line
       ws = words s'
@@ -38,7 +97,7 @@ parseLine s =
       to = last ws
       points = read (ws !! 3) :: Int
       sign = if "gain" `elem` ws then 1 else -1 -- "gain" is positive, "lose" negative
-   in (from, [SeatmateQuality to (points * sign)])
+   in (from, fromList [(to, points * sign)])
 
 neighbors :: [Person] -> [(Person, (Person, Person))]
 neighbors ps =
@@ -53,5 +112,10 @@ index xs n = xs !! (i `mod` length xs) where i = if n < 0 then length xs + n els
 map2 :: (a -> b) -> [[a]] -> [[b]]
 map2 = map . map
 
+happiness :: (Person, (Person, Person)) -> Int
+happiness (p, (pL, pR)) =
+  let prefs = preferences ! p
+   in (prefs ! pL) + (prefs ! pR)
+
 run :: IO ()
-run = print $ maximum $ map sum $ map2 (\(p, (pL, pR)) -> 123) $ map neighbors $ permutations $ keys preferences
+run = print $ maximum $ map sum $ map2 happiness $ map neighbors $ permutations $ keys preferences
