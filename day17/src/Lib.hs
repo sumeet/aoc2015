54c2271868d1e13eb49569cd1ccf47066b15e56a
Sumeet Agarwal <sumeet.a@gmail.com>
2021-01-27 18:18:13 -0800

    finish day17

--- day17/src/Lib.hs
+++ day17/src/Lib.hs
@@ -8,11 +8,23 @@ import Text.RawString.QQ
 sample :: IO ()
 sample = print $ numCombsTo 25 [20, 15, 10, 5, 5]
 
+part1 :: IO ()
+part1 = print $ numCombsTo 150 nums where nums = map read $ lines input
+
+part2 :: IO ()
+part2 = print $ length $ filter (\c -> length c == minLengthOfComb) combs
+  where
+    minLengthOfComb = minimum $ map length combs
+    combs = allCombs 150 (map read $ lines input)
+
 run :: IO ()
-run = print $ numCombsTo 150 nums where nums = map read $ lines input
+run = part2
+
+allCombs :: Int -> [Int] -> [[Int]]
+allCombs total nums = filter (\ns -> sum ns == total) $ subsequences nums
 
 numCombsTo :: Int -> [Int] -> Int
-numCombsTo total nums = length $ filter (\ns -> sum ns == total) $ subsequences nums
+numCombsTo total nums = length $ allCombs total nums
 
 input :: String
 input =
