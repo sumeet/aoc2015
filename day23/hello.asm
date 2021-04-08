eb9b5f42b0058fc2d0d2edbaa6293a04ec9d28c2
Sumeet Agarwal <sumeet.a@gmail.com>
2021-04-07 19:58:56 -0700

    wip

--- day23/hello.asm
+++ day23/hello.asm
@@ -1,3 +1,4 @@
+BITS 64;
 ; ----------------------------------------------------------------------------------------
 ; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
 ; To assemble and run:
